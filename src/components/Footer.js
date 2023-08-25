27e21228c2e664418725a5163df3788f25755c91
lucky favour <luckyfavour1219@gmail.com>
2023-08-24 18:29:22 -0700

    first commit

---
+++ src/components/Footer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Footer = () => {
+  const currentYear =new Date().getFullYear();
+  return <footer className='bg-black py-8 text-center text-white'>
+    <div className="container mx-auto">
+      Copyright &copy; {currentYear} . All rights reserved.
+    </div>
+  </footer>;
+};
+
+export default Footer;
